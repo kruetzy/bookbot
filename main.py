3b8f8c1b1a7a2b28b5bfde36a1f1a50806eac741
kruetzy <michael.kruetzkamp@gmail.com>
2025-02-16 20:36:26 -0600

    lesson 11

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         #print(file_contents)
-        word_count = count_words(file_contents)
-        sorted_dict = count_chars(file_contents)
-        print("Word Count:", word_count)
-        print(sorted_dict)
     
+    word_count = count_words(file_contents)
+    sorted_dict = count_chars(file_contents)
+    
+    isalpha_list = letter_count_list(sorted_dict)
+    isalpha_list.sort(key=lambda x: x[1], reverse=True)
+
+    # creating report
+    def print_report():
+        print("--- Begin report of books/frankenstein.txt --- \n")
+        print(f"{word_count} words found in the document \n")
+        for x in isalpha_list:
+            print(f"The '{x[0]}' character was found {x[1]} times")
+        print("\n --- End report ---")
+
+    print_report()
+
 def count_words(book):
     words = book.split()
     return len(words)
@@ -28,6 +40,11 @@ def count_chars(book):
     sorted_dict = dict(sorted(count_dict.items()))
     return sorted_dict
 
+def letter_count_list(dict):
+    isalpha_list = [(key, value) for key, value in dict.items() if key.isalpha()]
+    #print(isalpha_list)
+    return isalpha_list
+    
 
 
 main()
